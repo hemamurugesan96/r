0441f53ea10399a8bb2657760fc30c5f2f8c43b6
Hema M <hemamurugesanece@gmail.com>
2017-06-30 14:30:41 +0530

    Prime.java

---
+++ Prime.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import bjava.util.Scanner;
+class Prime
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+int count=0;
+for(int i=2;i<num;i++)
+{
+if(num%i==0)
+{
+count++;
+break;
+}
+}
+if(count==0)
+{
+System.out.println("Prime Number");
+}
+else
+{
+System.out.println("Not a Prime Number");
+}
+}
+}
